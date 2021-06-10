345efe7afd23524ad2368eda3407027c6ef5fd8e
scottming <therealscottming@gmail.com>
2021-06-10 15:23:00 +0800

    Add ex_doc deps

--- mix.exs
+++ mix.exs
@@ -56,6 +56,7 @@ defmodule EventBusDatastore.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:gen_stage, "~> 1.0"},
       {:postgrex, ">= 0.13.3"},
       {:ecto, "~> 3.0"},
